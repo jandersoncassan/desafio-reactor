44bafcaa13dfb150ba92cca7e4e742ad7fc337d0
Janderson Cassan <jande.max@gmail.com>
2024-11-03 13:24:25 -0300

    Desafio projeto Reactor

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -2,12 +2,25 @@ package br.com.dio.desafioreactor;
 
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
 public class Question3 {
 
     /*
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
+    public Mono<Void> userIsValid(final User user){
+
+        return Mono.just(user)
+                .filter(userData -> isPasswordOk(userData.password()))
+                .switchIfEmpty(Mono.defer(()-> Mono.error(new Exception(" Usuario invalido senha com menos de 8 caracteres"))))
+                .flatMap(data -> Mono.empty());
+    }
+
+    private boolean isPasswordOk(final String password){
+        return Objects.nonNull(password) && password.length() > 8;
+    }
 
 }
+
